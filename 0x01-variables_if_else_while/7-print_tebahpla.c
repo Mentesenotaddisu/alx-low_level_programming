bdb11eab584c26b81aedec0a2ce1f619267f9078
mentesenot <mentesenotaddisu@gmail.com>
2023-03-18 10:18:14 -0700

    h

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - entry
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'Z'; x >= 'A'; ++x)
+	{
+	putchar(tolower(x));
+	}
+	putchar('\n');
+	return (0);
+}
