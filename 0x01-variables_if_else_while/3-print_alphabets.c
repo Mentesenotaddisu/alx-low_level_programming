9d2f074beaa539baded1bbd5f3d95d9fd910e849
mentesenot <mentesenotaddisu@gmail.com>
2023-03-18 08:52:36 -0700

    g

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.>
+/**
+ * main - entry
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	putchar(tolower(x));
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	putchat(x);
+	}
+	putchat('\n');
+	return (0);
+
+}
