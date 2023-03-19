2b12f19c1cd13884a873f0614dfb33583dceae50
mentesenot <mentesenotaddisu@gmail.com>
2023-03-19 01:18:55 -0700

    g

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.>
+#include <ctype.h>
+/**
+ * main - entry
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; n++)
+	{
+	putchar(x);
+	if (n != 57)
+	{
+	putchat(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
