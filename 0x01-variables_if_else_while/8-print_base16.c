ad0b9a31ad041be369820b7d55ffc5da8157a528
mentesenot <mentesenotaddisu@gmail.com>
2023-03-19 00:30:03 -0700

    g

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - entry
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	 int x;
+
+	for (x = '0'; x <= '15'; x++)
+	{
+	putchar(x)
+	}
+	putchar('\n');
+	return (0);
+}
