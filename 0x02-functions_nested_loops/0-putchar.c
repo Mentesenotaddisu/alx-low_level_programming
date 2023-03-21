6007d623b534539414d4bcb80ef2b7469327a2c9
mentesenot <mentesenotaddisu@gmail.com>
2023-03-21 07:08:48 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,8 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-	_putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putchar(r);
-	_putchar('\n');
+	putchar(r);
+	putchar('\n');
 	return (0);
 }
 
