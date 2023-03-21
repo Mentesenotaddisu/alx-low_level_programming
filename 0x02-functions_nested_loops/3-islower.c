e76fcac28bf0a644db7e463597b618b7e0391c70
mentesenot <mentesenotaddisu@gmail.com>
2023-03-21 11:53:27 -0700

    h

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower -  entry
+ *
+ * Return: Always 0
+ */
+
+int _islower(int)
+{
+	int x;
+
+	for (x = _islower())
+	{
+	_putchar(1);
+	}
+	for (x = _isupper())
+	{
+	_putchar(0);
+	}
+	return (0);
+}
+
