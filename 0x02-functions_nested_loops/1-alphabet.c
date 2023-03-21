3ad15e0c42e2db82a4d7e5f7e3921b98ffb8b38f
mentesenot <mentesenotaddisu@gmail.com>
2023-03-21 08:46:09 -0700

    g

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - entry
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int x;
+
+	for (x = "a"; x <= "z"; x++)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+
+}
