0b38441fdb8c68d5fb238951ad005c738e10e38d
mentesenot <mentesenotaddisu@gmail.com>
2023-03-23 12:50:31 -0700

    g

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper- entry
+ * Return: Always 0
+ * @c: is char
+ */
+
+int _isupper(int c)
+{
+	if
+	{
+	_isupper(c);
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+	return (0);
+}
