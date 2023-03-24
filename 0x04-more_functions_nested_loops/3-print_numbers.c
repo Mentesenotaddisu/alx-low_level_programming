798f329e4496912946da6d27aab4e5effb56fbef
mentesenot <mentesenotaddisu@gmail.com>
2023-03-24 07:41:53 -0700

    h

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry
+ *
+ * Return: Always 0
+ *
+ */
+void print_numbers(void)
+{
+	for (x = 0; x <= 9; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
