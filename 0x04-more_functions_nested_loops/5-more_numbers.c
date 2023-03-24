d76e68ac5fef33843ae8b72771d4285e2776e7ba
mentesenot <mentesenotaddisu@gmail.com>
2023-03-24 09:29:00 -0700

    g

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,19 @@
 
 void more_numbers(void)
 {
-	int x;
+	int x, y;
 
-	for (x = 0; x <= 14; x++)
+	for (x = 0; x <= 10; x++)
 	{
-	_putchar(x);
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 0)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y %  10) + '0');
 	}
 	_putchar('\n');
-	return (10);
+	}
 }
 
