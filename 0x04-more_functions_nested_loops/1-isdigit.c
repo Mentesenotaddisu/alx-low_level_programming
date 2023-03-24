804994a3c05427f926ced678174723d9c582d0eb
mentesenot <mentesenotaddisu@gmail.com>
2023-03-24 07:23:52 -0700

    h

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isdigit - entry
+ *
+ *@c: checked
+ *
+ *Return: 1 if digit 0 else
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
