834c73c0046f60bb910a1319a92ba5d3c502ef20
mentesenot <mentesenotaddisu@gmail.com>
2023-03-24 07:29:26 -0700

    h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - entry
+ *
+ * @a: to be multiplied
+ * @b: to be multiplied
+ *
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	sum = a * b;
+	return (sum);
+}
+
