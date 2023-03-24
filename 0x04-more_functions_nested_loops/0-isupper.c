7691de39f09800425999f941d3569bc6487d9e3b
mentesenot <mentesenotaddisu@gmail.com>
2023-03-24 07:07:51 -0700

    h

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * _isupper - entry
- *
- * @x: check
- * Return: 1 for upper or 0 for else
- */
+* _isupper - entry
+*
+* @x: check
+* Return: 1 for upper or 0 for else
+*/
 
 int _isupper(int c)
 {
-
 	if (c >= 65 && c <= 98)
 	{
-	return (1);
-	}
 	return (0);
+	}
+	return (1);
 }
