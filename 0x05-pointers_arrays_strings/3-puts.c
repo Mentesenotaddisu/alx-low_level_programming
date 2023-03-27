88df388758f59aabf99d0379cb7cebceda9f7cfe
mentesenot <mentesenotaddisu@gmail.com>
2023-03-27 09:09:50 -0700

    h

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * _puts - entry
- * @str: string
- * Return: always 0
+ * @str: print
+ * Return: void
  */
 
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	for(; *str != '\0'; str++)
 	{
 	_putchar(*str);
 	}
