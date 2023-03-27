270f9f0a12349d9511caccabfa7f8d2e5141754c
mentesenot <mentesenotaddisu@gmail.com>
2023-03-27 07:40:18 -0700

    g

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - entry
+ * @a: int
+ * @b: int2
+ * Return : void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp:
+	x = y;
+	y = temp;
+}
