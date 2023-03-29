f9f174dbd7f0564b736c6e6744291d617f0c101f
mentesenot <mentesenotaddisu@gmail.com>
2023-03-29 10:19:40 -0700

    h

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strncpy - entry
+ * @dest: 1
+ * @src: 2
+ * @n: int
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i <= n ; i++)
+		dest[i] = src[i]
+	return (dest);
+}
