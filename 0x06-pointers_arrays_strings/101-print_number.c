7e50079b17232162c321b2e82cadd3cc45b40419
mentesenot <mentesenotaddisu@gmail.com>
2023-03-30 04:16:11 -0700

    oi

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,5 +6,16 @@
  */
 void print_number(int n)
 {
-	_putchar(n);
+	unsigned int n1;
+	n1 = n;
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
