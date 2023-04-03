81085a1f4a0d8b049acdf7b53f19adfb18593190
mentesenot <mentesenotaddisu@gmail.com>
2023-04-03 08:37:17 -0700

    h

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memcpy - entry 
+ *@dest: change
+ *@src: change
+ *@n: byte
+ *return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int y;
+
+	for (y = 0 ; y <= n; y++)
+	{
+	memcpy( dest[y], src[y], sizeof(src[y]));
+	}
+	return (dest);
+}
