bc4ac1e280823566de157d845d1b382c9763e460
mentesenot <mentesenotaddisu@gmail.com>
2023-04-05 13:23:58 -0700

    hello

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strlen_recursion - entry
+ *
+ *@s: string
+ *
+ *Return :string
+ */
+int _strlen_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++
+	return (_strlen_recursion(s) + 1);
+}
