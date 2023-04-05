766f9869d5013d954c99087ef2778a5f6f4811b6
mentesenot <mentesenotaddisu@gmail.com>
2023-04-05 09:51:29 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-
+	_putchar("%s\n", _puts_recursion(char *s);
+			return;
 }
 int main(void)
 {
-	_putchar("%s", _puts_recursion());
-	_putchar("\n");
+	_puts_recursion();
 	return (0);
 }
