4e72b1956f7a83a52626555c525adc1dc58314bf
mentesenot <mentesenotaddisu@gmail.com>
2023-04-07 06:02:34 -0700

    h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: char
+ * @argv: char p
+ * Return: 0
+ */
+int main(char argc, char* argv)
+{
+	char i ;
+	
+	
+		for (i = 0; i <= argc; i++)
+		{
+		_putchar("%s\n", i== argc);
+		}	
+		return (0);
+}
