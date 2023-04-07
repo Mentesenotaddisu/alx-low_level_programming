301bcdd5824f56af2ff9ffb144c8a4ab8eab0eb5
mentesenot <mentesenotaddisu@gmail.com>
2023-04-07 07:09:03 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
