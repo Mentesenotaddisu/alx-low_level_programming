ab538af5e3ba73ed5c0ea2d45f08379ccf483800
mentesenot <mentesenotaddisu@gmail.com>
2023-04-07 09:02:00 -0700

    g

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - entry
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x = 0;
+	unsigned int y, sum = 0;
+	char *e;
+
+	if (argc > 1)
+	{
+		for (x = 1; x < argc; x++)
+		{
+			e = argv[x];
+
+			for (y = 1; y < strlen(e); y++)
+			{
+			if (e[y] < 48 || e[y] > 57)
+			{
+			printf("Error\n");
+		return (1);
+			}
+			}
+		sum += atoi(e);
+	e++;
+		}
+	printf("%d\n", sum);
+	}	
+	else
+	{
+	printf("0\n");
+	}
+	return (0);
+}
