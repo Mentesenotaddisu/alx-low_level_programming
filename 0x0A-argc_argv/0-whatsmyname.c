0b18a08594032304362fc40e94ccd9ab3e67334d
mentesenot <mentesenotaddisu@gmail.com>
2023-04-07 07:01:38 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,10 @@
  * @argv: char p
  * Return: 0
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
+
 		return (0);
 }
