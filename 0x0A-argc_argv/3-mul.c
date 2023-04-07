7c461c79ed1f955e7f65895c131785b336cb0162
mentesenot <mentesenotaddisu@gmail.com>
2023-04-07 07:28:07 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x = 0 ;
+	int y = 0 ;
+
+	if (argc == 3)
+	{
+	x = atoi(argv[1];
+	y = atoi(argv[2];
+		printf("%d\n", x * y);
+	}
+	else
+	{
+	printf("Error\n");
+	return (1);
+	}
+	return (0);
+}
