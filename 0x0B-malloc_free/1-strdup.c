bcdd6d7cba53b5d4c6d5751146d5455a7e27c21c
mentesenot <mentesenotaddisu@gmail.com>
2023-04-10 12:24:23 -0700

    h

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,13 +12,15 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 	return (NULL);
-	 i = 0;
+
+	i = 0;
+
 	 while (str[i] != '\0')
 		 i++;
 	 x = malloc(sizeof(char) * (i + 1));
 	 if (x == NULL)
-		 return (NULL);
+	 return (NULL);
 	 for (y = 0; str[y]; y++)
-		 x[y] = str[y];
+	 x[y] = str[y];
 	 return (x);
 }
