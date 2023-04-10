62e750622a10898c4c2d81800a2e9d4dae455147
mentesenot <mentesenotaddisu@gmail.com>
2023-04-10 12:17:03 -0700

    h

---
+++ 0x0B-malloc_free/strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup - entry
+ *str: cha
+ *Return: NUll
+ */
+char *_strdup(char *str)
+{
+	char *x;
+	int i, y = 0;
+
+	if (str == NULL)
+	return (NULL);
+	 i = 0;
+	 while (str[i] != '\0')
+		 i++;
+	 x = malloc(sizeof(char) *(i + 1));
+	 if (x == NULL)
+		 return (NULL);
+	 for (y = 0; str[y]; y++)
+		 x[y] = str[r];
+	 return (x);
+}
