2b273c93014456fcdddff3d7b122f284d6f5bc8a
mentesenot <mentesenotaddisu@gmail.com>
2023-04-10 11:53:18 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,9 +7,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-	{
+	char *str;
+	unsigned int i ;
+
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || size == NULL)
 		return (NULL);
-	}
-	return;
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
