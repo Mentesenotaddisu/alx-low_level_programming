9d8fb544e45759d12ac352e187b28f1908ff7106
mentesenot <mentesenotaddisu@gmail.com>
2023-04-11 02:52:03 -0700

    h

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 	int i, j, k, l;
 	int **a;
 
-	if (width <= 0 || height <=)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	a = malloc(sizeof(int *) * height);
@@ -42,5 +42,5 @@ int **alloc_grid(int width, int height)
 			a[k][l] = 0;
 		}
 	}
-	return (a)
+	return (a);
 }
