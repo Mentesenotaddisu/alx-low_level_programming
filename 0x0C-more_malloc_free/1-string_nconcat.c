71edbfa9a3bb27641408de8bc99f07bfa5eff309
mentesenot <mentesenotaddisu@gmail.com>
2023-04-12 08:58:56 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *S;
+	char *s;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
@@ -32,6 +32,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s[i++] = s2[j++];
 	while (n > len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
-	s[i] = '\0'
+	s[i] = '\0';
 		return (s);
 }
