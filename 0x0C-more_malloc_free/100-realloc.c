174b429de7841bef518a51bb429c5fefdb383288
mentesenot <mentesenotaddisu@gmail.com>
2023-04-12 10:45:40 -0700

    task4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,11 +25,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	else
 	{
-	if (new_size ()
-	{
+	if (new_size == 0)
+		{
 	free (ptr);
 	return (NULL);
-	}
+		}
 	}
 	nptr = malloc (new_size);
 	if (nptr == NULL)
