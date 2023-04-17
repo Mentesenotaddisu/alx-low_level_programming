bbae4a7cbe547303482b818270364c3ba0be343e
mentesenot <mentesenotaddisu@gmail.com>
2023-04-17 11:04:24 -0700

    task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * init_dog - entr
+ * @d: ..
+ * @name: ..
+ * @age: ..
+ * @owner: ..
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d == malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
