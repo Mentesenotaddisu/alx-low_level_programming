f08a1f27d6c14a8047731fc6cccc1bb107933a98
mentesenot <mentesenotaddisu@gmail.com>
2023-04-17 11:40:50 -0700

    task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+*struct dog a dog's basic info
+*@name: First member
+*@age: Second member
+*@owner: Third member
+*Description: Longer description
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
