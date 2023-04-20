5bc3c0aa2033bce9434d6f039c513b30ba5d1fe1
mentesenot <mentesenotaddisu@gmail.com>
2023-04-20 09:49:34 -0700

    task2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+/**
+*print_numbers - returns sum
+*@n: count
+*@separator: comma
+*Return: void
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list args;
+char *str;
+
+va_start(args, n);
+
+if (separator == NULL)
+{
+return;
+}
+for (i = 0; i < n; i++)
+{
+	str = va_arg(args, char *);
+if (str == NULL)
+{
+	printf("nil");
+}
+else
+printf("%s", str);
+if (i != (n - 1) && separator != NULL)
+{
+printf("%s", separator);
+}
+}
+printf("\n");
+}
