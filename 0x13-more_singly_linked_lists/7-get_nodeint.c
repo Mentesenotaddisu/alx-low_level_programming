164e45f2cc4ac813d3bdea23db14d9c695b43fcb
mentesenot <mentesenotaddisu@gmail.com>
2023-05-02 08:33:01 -0700

    task7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *get_nodeint_at_index - entry
+ *@head: ..
+ *@index: ..
+ *Return: ..
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp = head;
+	unsigned int x;
+
+	while (temp && x < index)
+	{
+
+	temp = temp->next;
+	x++;
+	}
+	return (temp ? temp : NULL);
+}
